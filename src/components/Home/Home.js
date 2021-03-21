5f7555155fa44817919b0ff2b51f46d1348e4e6b
Farihaakter <yellowfariha@gmail.com>
2021-03-21 21:01:33 +0600

    added changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,16 +3,22 @@ import './Home.css';
 import Header from '../Header/Header';
 import ticket from '../../Data.folder/MOCK_DATA.json';
 import Ticket from '../Ticket/Ticket';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBiking } from '@fortawesome/free-solid-svg-icons'
 
 
 const Home = () => {
     const [tickets, setTickets] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         setTickets(ticket);
-    },[])
+    }, [])
     return (
         <div className="home">
             <Header></Header>
+            <div className='head-line'>
+                <h4>Choose Your Preferred Ri <FontAwesomeIcon  icon={faBiking} /> de</h4>
+                <p>We are always here for your rescue</p>
+            </div>
             <ul className="list">
                 {
                     tickets.map(ticket => <Ticket ticket={ticket} key={ticket.id}></Ticket>)
